e42dea12e5a8c43e9f63abd947a70bb5386da9f7
agnivchtj <agniv@physee.eu>
2022-05-09 15:58:41 +0200

    Update

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 const Environment = require('./models/environment');
 const Soil = require('./models/soil');
